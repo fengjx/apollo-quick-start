c14850e5b639da6cb83bddb0dce131dfd3a2afe8
铁匠 <fengjianxin2012@gmail.com>
2022-09-19 17:53:05 +0800

    fix

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -5,17 +5,17 @@ mkdir -p sql mysql
 compose_file="docker-compose.yml"
 if [ ! -f "$compose_file" ]; then
   echo "下载 docker-compose.yml"
-  curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/docker-compose.yml -o compose_file
+  curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/docker-compose.yml -o $compose_file
 fi
 
 configdb="sql/apolloconfigdb.sql"
-if [ ! -d "$configdb"]; then
+if [ ! -d "$configdb" ]; then
   echo "下载 ${configdb}"
   curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/sql/apolloconfigdb.sql -o ${configdb}
 fi
 
 portaldb="sql/apolloconfigdb.sql"
-if [ ! -d "$portaldb"]; then
+if [ ! -d "$portaldb" ]; then
   echo "下载 ${portaldb}"
   curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/sql/apolloportaldb.sql -o ${portaldb}
 fi
