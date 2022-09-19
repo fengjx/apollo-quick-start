783f74591d7b0e10f60535e9d18e6b97f3a9649c
铁匠 <fengjianxin2012@gmail.com>
2022-09-19 17:25:31 +0800

    fix

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-wget https://raw.githubusercontent.com/fengjx/apollo-quick-start/master/docker-compose.yml
+mkdir sql mysql
+
+curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/docker-compose.yml -o docker-compose.yml
+curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/sql/apolloconfigdb.sql -o sql/apolloconfigdb.sql
+curl -L https://raw.githubusercontent.com/fengjx/apollo-quick-start/dev/sql/apolloportaldb.sql -o sql/apolloportaldb.sql
+
 docker compose up -d
 
